d9233378734baca08b8eb6965286ddd5e5f01555
LATBauerdick <github@latbauerdick.de>
2019-04-28 10:35:15 -0500

    added qmk keymap configurations

--- qmk/keyboards/planck/keymaps/latbauerdick/keymap.c
+++ qmk/keyboards/planck/keymaps/latbauerdick/keymap.c
@@ -86,14 +86,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |  (/⇧ |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Ret/⇧ |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Lower| Spc/^|Esc/⌥ |Tab/⌘ |Spc/Lo| Space|Space |Spc/Rs|Left/⌘| Down |  Up  |->/Rse|
+ * | Raise| Spc/^|Esc/⌥ |Tab/⌘ |Spc/Lo| Space|Space |Spc/Rs|Left/⌘| Down |  Up  |->/Rse|
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
   CTL_ESC, NAV_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NAV_SCLN, TD(CT_QE),
   KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  RSF_RET,
-  LOWER,   CTL_SPC, ALT_ESC, GUI_TAB, LOW_SPC, LSF_SPC, RSF_SPC, RSE_SPC, GUI_LFT, KC_DOWN, KC_UP,    RSE_RGHT
+  RAISE,   CTL_SPC, ALT_ESC, GUI_TAB, LOW_SPC, LSF_SPC, RSF_SPC, RSE_SPC, GUI_LFT, KC_DOWN, KC_UP,    RSE_RGHT
 ),
 
 /* Colemak
@@ -177,7 +177,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_COMM, KC_DOT,  KC_P7,   KC_P8,   KC_P9,   KC_PAST, KC_MINS,
   RAISE,   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_QUOT,
   _______, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_BSLS,
-  _______, KC_BSPC, _______, _______, KC_LSFT, KC_BSPC, KC_SPC,  RSE_P0,  KC_P0,   KC_PDOT, KC_PENT, KC_GRV
+  RAISE,   KC_BSPC, _______, _______, KC_LSFT, KC_BSPC, KC_SPC,  RSE_P0,  KC_P0,   KC_PDOT, KC_PENT, KC_GRV
 ),
 
 /* Raise
@@ -195,7 +195,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_QUOT,
   _______, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  KC_GRV,  KC_BSLS, KC_QUOT, _______, _______, _______, KC_BSLS,
-  BACKLIT, _______, _______, _______, LOWER,   _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_GRV
+  BACKLIT, _______, _______, _______, KC_LSFT, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_GRV
 ),
 
 /* Directional navigation layer
