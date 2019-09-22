56bf3a4a0474b4b82aee75c90b416af819d6b96e
LATBauerdick <github@latbauerdick.de>
2019-09-22 15:54:34 -0500

    2019-09-23

--- qmk/keyboards/planck/keymaps/latbauerdick/keymap.c
+++ qmk/keyboards/planck/keymaps/latbauerdick/keymap.c
@@ -111,10 +111,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_grid(
-  _______, KC_Q,    KC_H,    KC_O,    KC_U,    KC_X,    KC_G,    KC_C,    KC_R,    KC_F,    KC_Z,    _______,
-  _______, KC_Y,    KC_I,    KC_E,    KC_A,    KC_DOT,  KC_D,    KC_S,    KC_T,    KC_N,    KC_B,    _______,
-  _______, KC_J,    KC_SLSH, KC_COMM, KC_K,    KC_QUOT, KC_W,    KC_M,    KC_L,    KC_P,    KC_V,    _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  _______, KC_Q,    KC_W,    KC_F,     KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, _______,
+  _______, NAV_A,   KC_R,    KC_S,     KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    NAV_O,   _______,
+  _______, KC_Z,    KC_X,    KC_C,     KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+  _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______
+  /* _______, KC_Q,    KC_H,    KC_O,    KC_U,    KC_X,    KC_G,    KC_C,    KC_R,    KC_F,    KC_Z,    _______, */
+  /* _______, KC_Y,    KC_I,    KC_E,    KC_A,    KC_DOT,  KC_D,    KC_S,    KC_T,    KC_N,    KC_B,    _______, */
+  /* _______, KC_J,    KC_SLSH, KC_COMM, KC_K,    KC_QUOT, KC_W,    KC_M,    KC_L,    KC_P,    KC_V,    _______, */
+  /* _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ */
 ),
 
 /* Colemak1: derived from Colemak Mod-DH, switching KM and rotating BGV
