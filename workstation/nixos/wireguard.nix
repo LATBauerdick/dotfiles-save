1149148e10dd25b766859cca9294da5385e87e8d
LATBauerdick <github@bauerdick.org>
2020-12-02 12:56:10 -0600

    2020-12-02

--- workstation/nixos/wireguard.nix
+++ workstation/nixos/wireguard.nix
@@ -17,15 +17,25 @@
     postSetup = ''
       ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o ens3 -j MASQUERADE
     '';
-
     postShutdown = ''
       ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.0/24 -o ens3 -j MASQUERADE
     '';
+# other routing options
+#  iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o ens3 -j SNAT --to-source 116.203.126.183
+#  iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE
+# port forwarding: make tcp service running on a client at 10.0.0.2:48000 accessible from the internet at 116.203.126.183:45000
+#  iptables -t NAT -A PREROUTING -d 116.203.126.183/32 -i ens3 -p tcp -m tcp --dport 45000 -j DNAT --to-destination 10.0.0.2:48000
+# A nginx reverse proxy may also be used as an alternative with the proxy_pass directive.
 
     peers = [
       {
         publicKey = "cP3x2nSm3P5jvrTqjEroG5iKIRCi6k5jwvgLvAsi6Cg=";
-        allowedIPs = [ "10.0.0.2/32" ];
+        allowedIPs = [ "10.0.0.0/24" "10.0.0.2/32" ];
+        persistentKeepalive = 25;
+      }
+      {
+        publicKey = "/xz3AXSHjuvNNDPxmvMWJIBNoTcEatfBldaS01EV1DM=";
+        allowedIPs = [ "10.0.0.3/32" ];
         persistentKeepalive = 25;
       }
     ];
