a8b8c45e5d2c3527cf847cb9e988cbb75e211838
LATBauerdick <github@bauerdick.org>
2020-12-01 20:21:13 -0600

    2020-12-01

--- qmk/keyboards/massdrop_alt/keymaps/latbauerdick/keymap.c
+++ qmk/keyboards/massdrop_alt/keymaps/latbauerdick/keymap.c
@@ -54,10 +54,10 @@ enum {
 #define RSE_WD   MO(_RSE_WD)
 #define LOW_WD   MO(_LOW_WD)
 #define NAV_A    LT(_NAV, KC_A)
-#define NAVWD_A  LT(_NAV_WD, KC_A)
 #define NAV_O    LT(_NAV, KC_O)
-#define NAVWD_O    LT(_NAV_WD, KC_O)
+#define NAVWD_A  LT(_NAV_WD, KC_A)
 #define NAV_SCLN LT(_NAV, KC_SCLN)
+#define NWD_QUOT LT(_NAV_WD, KC_QUOT)
 #define LOW_SPC  LT(_LOWER, KC_SPC)
 #define RSE_SPC  LT(_RAISE, KC_SPC)
 #define RWD_SPC  LT(_RSE_WD, KC_SPC)
@@ -77,16 +77,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT( /* QWERTY */
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, \
         KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
-        CTL_ESC, NAV_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NAV_SCLN,KC_QUOT,          KC_ENT,  KC_PGUP, \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_ENT,  \
-        LOWER,   KC_LALT, GUI_DEL,                            RSE_SPC,                            GUI_RET, MO(_ADJ),KC_LEFT, KC_DOWN, KC_RGHT  \
+        CTL_ESC, NAVWD_A, KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, NWD_QUOT,         KC_ENT,  KC_PGUP, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSF_SLS,          KC_UP,   KC_ENT,  \
+        LOW_WD , KC_LALT, GUI_DEL,                            RWD_SPC,                            GUI_RET, MO(_ADJ),KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [_COLEMAK] = LAYOUT( /* Colemak */
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        _______, KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, _______, _______, _______, _______, \
-        _______, _______, KC_R   , KC_S   , KC_T   , KC_G   , KC_M   , KC_N   , KC_E   , KC_I   , NAV_O  , _______,          _______, _______, \
-        _______, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_K   , KC_H   , _______, _______, _______, _______,          _______, _______, \
-        _______, _______, _______,                            _______,                            RSE_WD , _______, _______, _______, _______  \
+        _______, _______, _______, _______, _______, _______, _______, KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , _______, _______, \
+        _______, KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_LBRC, KC_RBRC, _______, _______, \
+        _______, NAV_A  , KC_R   , KC_S   , KC_T   , KC_G   , KC_M   , KC_N   , KC_E   , KC_I   , NAV_O  , KC_QUOT,          _______, _______, \
+        _______, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_K   , KC_H   , _______, _______, _______, KC_RSFT,          _______, _______, \
+        LOWER  , _______, _______,                            RSE_SPC,                            GUI_RET, _______, _______, _______, _______  \
     ),
     [_LOWER] = LAYOUT(
         KC_TILD, KC_EXLM, KC_AT ,  KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_7   , KC_8   , KC_9   , KC_ASTR, _______, _______, _______, _______, \
