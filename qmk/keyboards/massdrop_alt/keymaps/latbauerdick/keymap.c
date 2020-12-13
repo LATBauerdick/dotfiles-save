961f6d7e9f09b49fd1c8890be2ed81d6c1e83488
LATBauerdick <github@bauerdick.org>
2020-12-12 18:20:32 -0600

    2020-12-12

--- qmk/keyboards/massdrop_alt/keymaps/latbauerdick/keymap.c
+++ qmk/keyboards/massdrop_alt/keymaps/latbauerdick/keymap.c
@@ -53,14 +53,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
         CTL_ESC, NAVWD_A, KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, NWD_QUOT,         KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_ENT,  \
-        RAISE  , KC_LALT, GUI_DEL,                            RSE_SPC,                   GUI_RET, ADJ_SPC,          KC_LEFT, KC_DOWN, KC_RGHT  \
+        RAISE  , KC_LALT, GUI_DEL,                            RSE_SPC,                            GUI_RET, ADJ_SPC, KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [_CLMK_WD] = LAYOUT_65_ansi_blocker( /* Colemak wide layout */
         KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_EQL , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_BSPC, KC_BSPC, \
         KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_LBRC, KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_QUOT, KC_BSLS, KC_HOME, \
         CTL_ESC, NAVWD_A, KC_R   , KC_S   , KC_T   , KC_G   , KC_RBRC, KC_M   , KC_N   , KC_E   , KC_I   , NAVWD_O,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_X   , KC_C   , KC_D   , KC_V   , KC_Z   , KC_SLSH, KC_K   , KC_H   , KC_COMM, KC_DOT , RSF_SLS,          KC_UP,   KC_ENT , \
-        RSE_WD , KC_LALT, GUI_DEL,                            RWD_SPC,                            GUI_RET, MO(_ADJ),KC_LEFT, KC_DOWN, KC_RGHT  \
+        RSE_WD , KC_LALT, GUI_DEL,                            RWD_SPC,                            GUI_RET, ADJ_SPC, KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [_COLEMAK] = LAYOUT_65_ansi_blocker( /* Colemak */
         _______, _______, _______, _______, _______, _______, _______, KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , _______, _______, \
