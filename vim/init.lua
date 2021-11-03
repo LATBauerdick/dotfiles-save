b953d1ba04c038e51cacbb8bc6a9994e3686b033
LATBauerdick <github@bauerdick.org>
2021-11-02 22:20:26 -0500

    2021-11-02

--- vim/init.lua
+++ vim/init.lua
@@ -83,13 +83,13 @@ require('colors')
 
 --setup function for oberblastmeister/neuron.nvim
 -- these are all the default values
-require'neuron'.setup {
-    virtual_titles = true,
-    mappings = true,
-    run = nil, -- function to run when in neuron dir
-    neuron_dir = "~/zk", -- the directory of all of your notes, expanded by default (currently supports only one directory for notes, find a way to detect neuron.dhall to use any directory)
-    leader = "gz", -- the leader key to for all mappings, remember with 'go zettel'
-}
+-- require'neuron'.setup {
+--     virtual_titles = true,
+--     mappings = true,
+--     run = nil, -- function to run when in neuron dir
+--     neuron_dir = "~/zk", -- the directory of all of your notes, expanded by default (currently supports only one directory for notes, find a way to detect neuron.dhall to use any directory)
+--     leader = "gz", -- the leader key to for all mappings, remember with 'go zettel'
+-- }
 
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
