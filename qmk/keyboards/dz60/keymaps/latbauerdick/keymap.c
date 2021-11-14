853e0731433c10bfc5f462d5feafcf94c59805f6
LATBauerdick <github@bauerdick.org>
2021-11-13 18:40:22 -0600

    2021-11-13

--- qmk/keyboards/dz60/keymaps/latbauerdick/keymap.c
+++ qmk/keyboards/dz60/keymaps/latbauerdick/keymap.c
@@ -15,32 +15,35 @@ enum alt_layers {
 #define RSF_ENT  MT(MOD_RSFT, KC_ENT)
 #define LSF_Z    MT(MOD_RSFT, KC_Z)
 #define GUI_SPC  MT(MOD_LGUI, KC_SPC)
+#define GUI_ENT  MT(MOD_LGUI, KC_ENT)
 #define RGU_SPC  MT(MOD_RGUI, KC_SPC)
+#define ALT_ESC  MT(MOD_LALT, KC_ESC)
 #define RSE_SPC  LT(_RAISE, KC_SPC)
 #define ADJ_SPC  LT(_ADJ, KC_SPC)
+#define ADJ_ENT  LT(_ADJ, KC_ENT)
 #define NAV_A    LT(_NAV, KC_A)
 #define NAV_O    LT(_NAV, KC_O)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT(
-          KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_GRV , KC_7,    KC_8,    KC_9,    KC_0   , KC_MINS, KC_EQL , KC_BSPC,
+          KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0   , KC_MINS, KC_EQL , KC_GRV , KC_BSPC,
           KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_LBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT, KC_BSPC,
-          CTL_ESC, NAV_A,   KC_R,    KC_S,    KC_T,    KC_G,    KC_RBRC, KC_M,    KC_N,    KC_E,    KC_I,    KC_O   ,          KC_ENT,
- KC_LSFT, LSF_Z  , KC_X,    KC_C,    KC_D,    KC_V,    KC_Z,    KC_SLSH, KC_K,    KC_H,    KC_COMM, KC_DOT,           RSF_SLS,          ADJ_SPC,
-          RAISE  , KC_LALT, GUI_SPC,          RSE_SPC,          RSE_SPC,          KC_SPC ,          RGU_SPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+          CTL_ESC, NAV_A,   KC_R,    KC_S,    KC_T,    KC_G,    KC_SCLN, KC_M,    KC_N,    KC_E,    KC_I,    KC_O   ,          KC_ENT,
+ KC_LSFT, LSF_Z  , KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_SLSH, KC_K,    KC_H,    KC_COMM, KC_DOT,           RSF_ENT,          ADJ_ENT,
+          KC_CAPS, ALT_ESC, GUI_ENT,          GUI_SPC,          RSE_SPC,          RSE_SPC,          RGU_SPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [_RAISE] = LAYOUT(
          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-         KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS,          \
-         _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , XXXXXXX, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,          _______,          \
-_______, _______, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL , KC_LPRN, KC_BSPC, KC_RPRN, KC_QUOT, _______, _______,          _______,          _______, \
-         _______, _______, _______,          _______,          KC_BSPC,          _______,          _______, _______, _______, _______, _______  \
+         KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_RBRC, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_LBRC, KC_RBRC, _______,          \
+         _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_QUOT, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,          _______,          \
+_______, _______, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL , KC_LPRN, KC_BSLS, KC_RPRN, KC_QUOT, _______, _______,          _______,          _______, \
+         _______, _______, _______,          _______,          _______,          _______,          _______, _______, _______, _______, _______  \
     ),
     [_NAV] = LAYOUT(
           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_J), XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_BSLS,
-          _______, XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END , XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,          _______,
- _______, _______, G(KC_X), G(KC_C), G(KC_D), G(KC_V), G(KC_Z), KC_BSPC, KC_LEFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          _______,
+          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RBRC, S(KC_J), XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_BSLS,
+          _______, XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END , KC_COLN, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,          _______,
+ _______, _______, G(KC_Z), G(KC_X), G(KC_C), G(KC_D), G(KC_V), KC_BSPC, KC_BSPC, KC_LEFT, XXXXXXX, XXXXXXX, XXXXXXX,          _______,
           _______, _______, _______,          _______,          KC_BSPC,          _______,          _______, KC_BRMD, KC_VOLD, KC_VOLU, KC_BRMU
     ),
     [_ADJ] = LAYOUT(
