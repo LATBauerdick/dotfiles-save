670092887f9edc89eb035da63673bab4b9a3f659
LATBauerdick <github@bauerdick.org>
2021-11-18 17:21:21 -0600

    2021-11-18

--- qmk/keyboards/dz60/keymaps/latbauerdick/keymap.c
+++ qmk/keyboards/dz60/keymaps/latbauerdick/keymap.c
@@ -28,7 +28,7 @@ enum alt_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT(
           KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0   , KC_MINS, KC_EQL , KC_GRV , KC_BSPC,
-          KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_LBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT, KC_BSPC,
+          KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_LBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, KC_RBRC, KC_BSPC,
           CTL_ESC, NAV_A,   KC_R,    KC_S,    KC_T,    KC_G,    KC_SCLN, KC_M,    KC_N,    KC_E,    KC_I,    KC_O   ,          KC_ENT,
  KC_LSFT, LSF_Z  , KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_SLSH, KC_K,    KC_H,    KC_COMM, KC_DOT,           RSF_ENT,          ADJ_ENT,
           KC_CAPS, ALT_ESC, GUI_ENT,          GUI_SPC,          RSF_SPC,          RSE_SPC,          RGU_SPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
